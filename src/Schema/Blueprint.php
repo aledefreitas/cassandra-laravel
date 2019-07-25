ef6e68980b8db93887691be9e2609cc9f41d257b
Alexandre Caetano <alexandrefc2@hotmail.com>
2019-07-25 09:53:40 -0300

    Fixes schema blueprint for laravel 5.8

--- src/Schema/Blueprint.php
+++ src/Schema/Blueprint.php
@@ -50,7 +50,7 @@ class Blueprint extends BaseBluprint
      */
     public function toSql(Connection $connection, BaseGrammar $grammar)
     {
-        $this->addImpliedCommands();
+        $this->addImpliedCommands($grammar);
 
         $statements = [];
         // Each type of command has a corresponding compiler function on the schema
@@ -243,7 +243,7 @@ class Blueprint extends BaseBluprint
      *
      * @return \Illuminate\Support\Fluent
      */
-    public function timestamp($column)
+    public function timestamp($column, $precision = 0)
     {
         return $this->addColumn('timestamp', $column);
     }
