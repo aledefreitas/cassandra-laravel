e13af8932568d1e4cafe42afeacbbc2e4e259240
Alexandre Caetano <alexandrefc2@hotmail.com>
2019-11-19 09:50:28 +0000

    Revert "Adds allow filtering to delete and update"
    This reverts commit 39e665eae37a412d79cb457adc6efe990086cee9.

--- src/Query/Grammar.php
+++ src/Query/Grammar.php
@@ -74,9 +74,8 @@ class Grammar extends BaseGrammar
         }
 
         $wheres = is_array($query->wheres) ? $this->compileWheres($query) : '';
-        $allowFiltering = $query->allowFiltering ? 'ALLOW FILTERING' : '';
 
-        return trim("delete {$delColumns} from {$this->wrapTable($query->from)} {$wheres} {$allowFiltering}");
+        return trim('delete '.$delColumns." from {$this->wrapTable($query->from)} $wheres");
     }
 
     /**
@@ -108,9 +107,7 @@ class Grammar extends BaseGrammar
             $upateCollections = $columns ? ', '.$upateCollections : $upateCollections;
         }
 
-        $allowFiltering = $query->allowFiltering ? 'ALLOW FILTERING' : '';
-
-        return trim("update {$table} set $columns $upateCollections $wheres {$allowFiltering}");
+        return trim("update {$table} set $columns $upateCollections $wheres");
     }
 
     /**
